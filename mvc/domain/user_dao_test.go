5ccf2d4d5e14a79aeb7eea93b234e121714a12d3
Luis Antonio Feregrino <djlaf2008@hotmail.com>
2022-01-11 12:04:42 -0600

    Adding benchmarks

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	// Initialization
+
+	user, err := GetUser(0)
+	if user != nil {
+		t.Error("We are not expecting a user with UserID 0")
+	}
+
+	if err == nil {
+		t.Error("We are expecting a user with UserID 0")
+	}
+
+	if err.StatusCode != http.StatusNotFound {
+		t.Error("We are expecting 404 for a user with UserID 0")
+	}
+}
